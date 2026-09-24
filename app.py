be9926dfe8d67e22a542b910f1f8367e1b34a5c8
Hmitsev <hmitsev@intercars.eu>
2026-09-24 12:31:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1313,17 +1313,17 @@ if page == "📄 PDF → Excel":
             f"{invalid_total_count}"
         )
 
-       preview_df = final_result_df[
-            [
-                "Cross-Reference Type No.",
-                "Internal Item No.",
-                "Item No.",
-                "Cross-Reference No.",
-                "Qty",
-                "Price 1 pc"
-            ]
-        ]
-].copy()
+               preview_df = final_result_df[
+                    [
+                        "Cross-Reference Type No.",
+                        "Internal Item No.",
+                        "Item No.",
+                        "Cross-Reference No.",
+                        "Qty",
+                        "Price 1 pc"
+                    ]
+                ]
+    ].copy()
 
         st.subheader(
             "📋 Разпознати фактурни позиции"
